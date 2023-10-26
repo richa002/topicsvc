841f045d0042b61dc5f97abec5ff19dbb95ce87a
Richa Luthra <richaluthra.ca@gmail.com>
2023-10-26 12:15:51 -0400

    initial  commit

---
+++ src/main/java/com/softech/topicsvc/controller/TopicController.java
@@ -0,0 +1,15 @@
+package com.softech.topicsvc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/topicsvc/v1")
+public class TopicController {
+    //dummy endpoint
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!!!";
+    }
+}
